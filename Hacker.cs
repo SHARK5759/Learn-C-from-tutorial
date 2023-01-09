9adf38fab972a3872a56af318ad41d4c9eb0b659
SHARK5759 <121607662+SHARK5759@users.noreply.github.com>
2023-01-09 11:00:37 +0800

    Add files via upload

---
+++ Hacker.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    //Game state
+    int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello L");
+        Terminal.WriteLine("What would you like to hack into?\n");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for the NASA\n");
+        Terminal.WriteLine("Enter your selection");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("wrong number");
+        }
+            
+        
+    }
+    private void StartGame()
+    {
+        Terminal.WriteLine("You have chosen level"+level);
+    }
+}
